eacecb21e9c67f171485a2362d2ab343d11315ca
KER AND HAN <shalcozbi@gmail.com>
2025-03-13 00:40:04 +0800

    pseudocode batch3 prog4

---
+++ batch_3/display_lowest_number.py
@@ -0,0 +1,7 @@
+#Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
+
+#create an empty list that stores infinite inputs until invalid
+#    get user input as an integer
+
+# if the user input is invalid:
+    #print invalid input and the lowest number entered and stop the loop
