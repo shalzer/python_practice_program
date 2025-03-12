ff5d688c2ac69d526c03d164b95a97401153b0a5
KER AND HAN <shalcozbi@gmail.com>
2025-03-12 18:41:21 +0800

    pseudocode

---
+++ Batch 1/exponentiation_of_two_numbers.py
@@ -0,0 +1,6 @@
+#Prog06: Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
+
+#Asks the user to input the 1st number (could be a decimal)
+#ask user to input 2nd number
+#Calculates the two inputted numbers when the first number is raised to the 2nd inputted number
+#Prints the result
