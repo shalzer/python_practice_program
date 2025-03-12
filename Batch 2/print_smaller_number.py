efc8ab707096a1827b5917d672ae89295944a5fb
KER AND HAN <shalcozbi@gmail.com>
2025-03-12 20:05:47 +0800

    prog1: compares two number and prints the smaller number

--- Batch 2/print_smaller_number.py
+++ Batch 2/print_smaller_number.py
@@ -4,3 +4,12 @@
 # check if num1 is smaller than num2
 # print num1 if it's smaller
 # print num2 if not
+
+num1 = int(input("Enter 1st number: "))
+num2 = int(input("Enter 2nd number: "))
+
+if num1 < num2:
+    print(num1)
+
+else:
+    print(num2)
