7ab597b19665bf8906827aa03098b9de1f42baa2
KER AND HAN <shalcozbi@gmail.com>
2025-03-12 20:17:46 +0800

    pseudocode batch2 prog3

---
+++ Batch 2/print_difference_of_2_numbers.py
@@ -0,0 +1,5 @@
+#Prog03: Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
+
+#Ask the user to input 2 numbers (could be a decimal)
+#Compute the difference of the two numbers
+#Print the difference
