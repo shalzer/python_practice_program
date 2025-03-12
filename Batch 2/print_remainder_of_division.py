26080a09ec325bac83c7c50a75a4ddaa5b6dac32
KER AND HAN <shalcozbi@gmail.com>
2025-03-12 20:32:15 +0800

    prog06: calculates and prints the remainder by using modulous operator to get the remainder of the two numbers

--- Batch 2/print_remainder_of_division.py
+++ Batch 2/print_remainder_of_division.py
@@ -4,3 +4,8 @@
 # Ask the user to input the second number
 # Compute the remainder of the first number divided by the second number
 # Print the remainder
+
+num1 = int(input("Please enter a number: "))
+num2 = int(input("Enter 2nd number: "))
+
+print(f"The remainder of the quotient is: ", num1 % num2)
