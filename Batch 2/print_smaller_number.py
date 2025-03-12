f2ea45de1d42bb092ff5da243c255e0d12f218d3
KER AND HAN <shalcozbi@gmail.com>
2025-03-12 20:04:30 +0800

    fixed typo error

--- Batch 2/print_smaller_number.py
+++ Batch 2/print_smaller_number.py
@@ -1,6 +1,6 @@
 #Prog01: Create a program that ask user to input 2 numbers. Print the smaller number.
 
 # ask user to input num1 and num2
-# check if num1 is greater than num2
-# print num1 if it's greater
+# check if num1 is smaller than num2
+# print num1 if it's smaller
 # print num2 if not
