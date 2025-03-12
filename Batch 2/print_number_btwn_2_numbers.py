49882e7d64135e51752296c59003b7fc013f7e6f
KER AND HAN <shalcozbi@gmail.com>
2025-03-12 21:50:25 +0800

    pseudocode batch2 prog10

---
+++ Batch 2/print_number_btwn_2_numbers.py
@@ -0,0 +1,7 @@
+#Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
+
+# ask for the first number from user (num1)
+# ask for second number from user (num2)
+# identify the smaller and larger number
+# Loop from the smaller number + 1 to the larger number - 1
+#   Print each number in the range  
