2ea7ea0c47609ed981a66b0f39ba24cf0bbe14c8
KER AND HAN <shalcozbi@gmail.com>
2025-03-12 20:39:07 +0800

    pseudocode batch2 prog6

---
+++ Batch 2/first_number_minus_remaining_numbers.py
@@ -0,0 +1,9 @@
+#Prog06: Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
+
+#initialize diff to 0
+#make the first number a variable, ask the user to enter a number
+#loop 9 numbers to enter remaining 9 numbers:
+#   Ask user to enter a number
+#   add number to diff variable
+#subtract diff from num1
+#print result
