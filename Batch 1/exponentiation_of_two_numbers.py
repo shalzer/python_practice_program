c67c5dd9483436a9d69ea47f8e6d02c367de9bee
KER AND HAN <shalcozbi@gmail.com>
2025-03-12 18:44:09 +0800

    prog6: created a program that calculates andprints the result when the 1st number is raised to the 2nd inputted number.

--- Batch 1/exponentiation_of_two_numbers.py
+++ Batch 1/exponentiation_of_two_numbers.py
@@ -1,6 +1,11 @@
 #Prog06: Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
 
 #Asks the user to input the 1st number (could be a decimal)
-#ask user to input 2nd number
+#Ask user to input 2nd number
 #Calculates the two inputted numbers when the first number is raised to the 2nd inputted number
 #Prints the result
+
+num1 = float(input("Please enter the 1st number: "))
+num2 = float(input("Please enter the 2nd number: "))
+
+print("The result is: ", num1 ** num2)
