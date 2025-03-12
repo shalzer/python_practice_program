e07f2eeb293955865af9815e62f817ff78bbf791
KER AND HAN <shalcozbi@gmail.com>
2025-03-13 02:09:44 +0800

    pseudocode batch4 prog3

---
+++ batch_4/display_highest_number.py
@@ -0,0 +1,9 @@
+#Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the highest number
+
+#create an empty list that stores infinite inputs until invalid
+#    get user input as an integer
+#    stores the number entered on the list
+
+# if the user input is invalid:
+    #the program goes through the list and check the highest number
+    #print invalid input and the highest number entered and stop the loop
