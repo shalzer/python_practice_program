67cd78ddfd954e8b25d110706954ec9e9bc27174
KER AND HAN <shalcozbi@gmail.com>
2025-03-12 18:26:51 +0800

    pseudocode batch1 prog4

---
+++ Batch 1/print_product_of_two_numbers.py
@@ -0,0 +1,6 @@
+#Prog04: Create a program that ask user to input 2 numbers. Print the product of the two numbers.
+
+#Asks the user to input the 1st number (could be a decimal)
+#ask user to input 2nd number
+#Compute the product of the two inputted numbers
+#Prints the product
