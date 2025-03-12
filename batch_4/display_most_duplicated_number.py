6b9b871add8a9679899428c480192435a3c39655
KER AND HAN <shalcozbi@gmail.com>
2025-03-13 01:59:26 +0800

    pseudocode batch4 prog2

---
+++ batch_4/display_most_duplicated_number.py
@@ -0,0 +1,8 @@
+#Prog02: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number with the most number of duplicate.
+
+# Create an empty list to store infinite numbers
+# Start an infinite loop
+#    get user input in integer
+#    add it to the list (numbers)
+# create variable that checks the frequency of all the entered numbers
+# if the user input is invalid, the program will terminate and will print the most frequent number that appeared"
