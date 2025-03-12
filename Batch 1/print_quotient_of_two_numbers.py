cb5ad0b4470cde64720a3cf9e011f2e0925efedf
KER AND HAN <shalcozbi@gmail.com>
2025-03-12 18:36:27 +0800

    pseudocode batch1 prog5

---
+++ Batch 1/print_quotient_of_two_numbers.py
@@ -0,0 +1,6 @@
+#Prog05: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
+
+#Asks the user to input the 1st number (could be a decimal)
+#ask user to input 2nd number
+#Compute the quotient of the two inputted numbers
+#Prints the quotient
