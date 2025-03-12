c72333aaff01cfbe0b06aa2d2ef4938e0218c0a4
KER AND HAN <shalcozbi@gmail.com>
2025-03-12 19:19:49 +0800

    pseudocode batch1 prog9

---
+++ Batch 1/print_even_0_100.py
@@ -0,0 +1,5 @@
+#Prog09: Create a program that print all the even numbers starting from 0 to 100. (Use for loop)
+
+#Loop numbers from 0 to 100
+#Increase by 2 to get the even numbers
+#print the even numbers
--- Batch 1/print_odd_numbers.py
+++ Batch 1/print_odd_numbers.py
@@ -13,4 +13,4 @@ for i in range(10):
     number = int(input(f"Enter number {i + 1}: "))
     if number % 2 == 1:
         odd_count += 1
-print("Total odd numbers:", odd_count)
+print("Total odd  numbers:", odd_count)
