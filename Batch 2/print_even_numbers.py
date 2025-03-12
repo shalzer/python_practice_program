985d88c61f3d091b86da33ce71ad19fd0f331b67
KER AND HAN <shalcozbi@gmail.com>
2025-03-12 20:50:10 +0800

    asks user 10 numbers, loops the number 10x. checks which are even, prints it

--- Batch 2/print_even_numbers.py
+++ Batch 2/print_even_numbers.py
@@ -6,3 +6,12 @@
 #    Check if the number is even by: number divided by 2 == 0
 #    If true, increase even_count by 1
 # Print the total count of even numbers
+
+even_count = 0
+
+for i in range(10):
+    number = int(input(f"Enter number {i + 1}: "))
+    if number % 2 == 0:
+        even_count += 1
+
+print("Even numbers count:", even_count)
