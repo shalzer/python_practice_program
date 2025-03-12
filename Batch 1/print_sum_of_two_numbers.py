39f2f6120c006f592e3b8818569d577a0114edee
KER AND HAN <shalcozbi@gmail.com>
2025-03-12 18:17:58 +0800

    pseudocode batch1 prog3

---
+++ Batch 1/print_sum_of_two_numbers.py
@@ -0,0 +1,5 @@
+#Prog03: Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
+
+#Ask the user to input 2 numbers (could be a decimal)
+#Compute the sum of the two numbers
+#Print the sum
