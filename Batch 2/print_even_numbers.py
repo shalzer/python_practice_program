55ab23af05ba17d406c5d50fe959d075864cb060
KER AND HAN <shalcozbi@gmail.com>
2025-03-12 20:47:48 +0800

    pseudocode batch2 prog7

---
+++ Batch 2/print_even_numbers.py
@@ -0,0 +1,8 @@
+#Prog07: Create a program that ask user to input 10 numbers. Print how many are even numbers.
+
+# Initialize even_count to 0
+# Loop 10 times to get user input
+#    Ask the user to enter a number
+#    Check if the number is even by: number divided by 2 == 0
+#    If true, increase even_count by 1
+# Print the total count of even numbers
