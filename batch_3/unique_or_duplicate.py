c468390b2c94457684324682aacbb3a86f29596a
KER AND HAN <shalcozbi@gmail.com>
2025-03-13 00:14:27 +0800

    pseudocode batch3 prog3

---
+++ batch_3/unique_or_duplicate.py
@@ -0,0 +1,15 @@
+#Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display "Unique" after input when the inputted number don't have duplicate. Display "Duplicate" after input when the inputted number have duplicate.
+
+#create an empty list to store entered numbers
+
+#start an infinite loop that ask for user input
+#    try to get user input as an integer
+
+# if the number is already in the list
+    #print "Duplicate"
+# else
+    # print "Unique"
+    # sdd the number to the list
+
+# if the input is not a valid integer (invalid input)
+    # print an error message and terminate the program
