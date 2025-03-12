95a369387dd3ab99cdc96329fd7de2527ee49fbf
KER AND HAN <shalcozbi@gmail.com>
2025-03-12 19:00:30 +0800

    pseudocode batch1 prog8

---
+++ Batch 1/print_odd_numbers.py
@@ -0,0 +1,8 @@
+#Prog08: Create a program that ask user to input 10 numbers. Print how many are odd numbers.
+
+# Initialize odd_count to 0
+# Loop 10 times to get user input
+#    Ask the user to enter a number
+#    Check if the number is odd by: number divided by 2 == 1
+#    If true, increase odd_count by 1
+# Print the total count of odd numbers
