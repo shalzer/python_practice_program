091d1ecf949655d5a8b7dd39c2e1551dc1dccced
KER AND HAN <shalcozbi@gmail.com>
2025-03-12 20:27:47 +0800

    pseudocode batch2 prog5

---
+++ Batch 2/print_remainder_of_division.py
@@ -0,0 +1,6 @@
+#Prog05: Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number.
+
+# Ask the user to input the first number
+# Ask the user to input the second number
+# Compute the remainder of the first number divided by the second number
+# Print the remainder
