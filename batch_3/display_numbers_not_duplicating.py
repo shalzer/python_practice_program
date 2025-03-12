513fea79e06f517bbbd226fc954da18f623e98f7
KER AND HAN <shalcozbi@gmail.com>
2025-03-12 23:25:09 +0800

    pseudocode batch3 prog1

--- batch_3/display_numbers_not_duplicating.py
+++ batch_3/display_numbers_not_duplicating.py
@@ -1,6 +1,12 @@
 #Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
 
-#create a loop that ask user to enter a number
-#
+#create an empty list
+#create a loop that ask user to enter a number 10x
+#the entered numbers r stored in the list (numbers)
 
-number
+#create another list for not repeating numbers
+#  checks if numbers stored in both list does not repeat through looping
+#  if it does not repeat:
+#     add it to the 2nd list (unique_num)
+
+#prints out the number
