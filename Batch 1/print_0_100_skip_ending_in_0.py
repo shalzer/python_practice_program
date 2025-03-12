e49fbc48aebc55ce4f1337a455b936beeebe5393
KER AND HAN <shalcozbi@gmail.com>
2025-03-12 19:28:16 +0800

    pseudocode batch1 prog10

---
+++ Batch 1/print_0_100_skip_ending_in_0.py
@@ -0,0 +1,7 @@
+#Prog10: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero.
+
+#initialize num to 0
+#loop while num less than or equal to 0
+#  check if num does not end in 0
+#    if true, print the number
+#  increase num by 1
