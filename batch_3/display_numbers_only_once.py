d7eddac62f63d61fa96c38d1c2842391e830e77f
KER AND HAN <shalcozbi@gmail.com>
2025-03-13 00:01:02 +0800

    pseudocode batch3 prog2

---
+++ batch_3/display_numbers_only_once.py
@@ -0,0 +1,9 @@
+
+#crate a list that stores non repeating numbers
+#create another list for reiterating numbers
+#Loop 10x to get user's input
+# if the number has not been entered before:
+#   add it to non-repeating list
+#   mark the number as seen by adding it to the repeating number list
+
+#print the list of number that without duplicating any
