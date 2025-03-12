29aa66d336f31b0c10dba8cdcb5fdb27b21d9d19
KER AND HAN <shalcozbi@gmail.com>
2025-03-13 01:07:31 +0800

    pseudocode batch4 prog1

---
+++ batch_4/print_duplicating_numbers.py
@@ -0,0 +1,12 @@
+#Prog01: Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
+
+#create an empty list
+#create a loop that ask user to enter a number 10x
+#the entered numbers r stored in the list (numbers)
+
+#create another list for repeating numbers
+#  checks if numbers stored in both list does repeat through looping
+#  if it repeats:
+#     add it to the 2nd list (duplicate_num)
+
+#prints out the duplicate number
