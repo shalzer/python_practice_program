947d61ba74015bc443dd7e39c99cd7766930a3df
KER AND HAN <shalcozbi@gmail.com>
2025-03-12 18:49:02 +0800

    pseuodocode batch1 prog7

---
+++ Batch 1/prints_sum_of_ten_input_number.py
@@ -0,0 +1,7 @@
+#Prog07: Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
+
+# Initialize total to 0
+# Loop 10 times to get user input
+#    Ask the user to enter a number 10 times
+#    Add the number to the total
+# Print the sum of all numbers
