8b803d94d118987fb1f131f5871cc529e5e1bceb
KER AND HAN <shalcozbi@gmail.com>
2025-03-12 20:19:14 +0800

    asks the user to input 2 numbers, calculates the difference of it, prints the difference

--- Batch 2/print_difference_of_2_numbers.py
+++ Batch 2/print_difference_of_2_numbers.py
@@ -3,3 +3,8 @@
 #Ask the user to input 2 numbers (could be a decimal)
 #Compute the difference of the two numbers
 #Print the difference
+
+num1 = float(input("Please enter the 1st number: "))
+num2 = float(input("Please enter the 2nd number: "))
+
+print(f"The difference of the 2 numbers is: ", num1 - num2)
