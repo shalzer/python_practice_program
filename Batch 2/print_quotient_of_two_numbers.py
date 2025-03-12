7888366b4deff800533a47cd1dd3371ffdbaf1b9
KER AND HAN <shalcozbi@gmail.com>
2025-03-12 20:21:51 +0800

    pseuodocode batch2 prog4

---
+++ Batch 2/print_quotient_of_two_numbers.py
@@ -0,0 +1,5 @@
+#Prog04: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without decimal point.
+
+#Ask the user to input 2 numbers (could be a decimal)
+#Compute the quotient of the two numbers
+#Print the quotient
