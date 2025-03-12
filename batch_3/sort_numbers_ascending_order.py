1192438b2ddb37e578e868822d4e60bbaa60a08c
KER AND HAN <shalcozbi@gmail.com>
2025-03-13 00:55:24 +0800

    pseudocode batch3 prog5

---
+++ batch_3/sort_numbers_ascending_order.py
@@ -0,0 +1,9 @@
+#Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from lowest to highest. Clue: sort() function
+
+#create an empty list that stores infinite numbers entered
+
+#start an infinite loop that ask for user input
+#    try to get user input as an integer
+
+# if the user input is invalid:
+    #print invalid input and the ascending order of number entered and stop the loop
